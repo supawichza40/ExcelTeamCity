31009af41a982d83614a2d60b175e5e8e8b9ac49
supawichza40 <supawichza@gmail.com>
2022-09-10 22:30:56 +0100

    added html and css

--- pandasDF2.py
+++ pandasDF2.py
@@ -3,8 +3,13 @@ import pandas as pd
 df = pd.read_excel("text.xlsx")
 df["calories"][0] =df["calories"][0]+1000
 df.to_excel("text2.xlsx")
-html = df.to_html()
+
+
+def style_negative(v, props=''):
+    return props if v >300 else None
+s2 = df.style.applymap(style_negative, props='color:red;')
+html = s2.to_html()
 text_file = open("index.html","w")
 text_file.write(html)
 text_file.close()
-print(df)
+
