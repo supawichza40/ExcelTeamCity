8a2b56711524f5f1bd527b2bbb89eb33ad11e307
supawichza40 <supawichza@gmail.com>
2022-09-10 22:12:22 +0100

    added html file

--- pandasDF2.py
+++ pandasDF2.py
@@ -3,4 +3,8 @@ import pandas as pd
 df = pd.read_excel("text.xlsx")
 df["calories"][0] =df["calories"][0]+1000
 df.to_excel("text2.xlsx")
+html = df.to_html()
+text_file = open("index.html","w")
+text_file.write(html)
+text_file.close()
 print(df)
